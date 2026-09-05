cb4867d861299cbc5e5b464ac54d8674a48ba61a
Arkaelkernel <151031855+Arkael-Dev@users.noreply.github.com>
2026-09-05 12:08:41 +0700

    Update RELEASE_REPO for GitHub Release Configuration

--- config.sh
+++ config.sh
@@ -25,7 +25,7 @@ CLANG_URL="https://github.com/Neutron-Toolchains/clang-build-catalogue/releases/
 # ==========================================
 # 4. GitHub Release Configuration
 # ==========================================
-RELEASE_REPO="Arkael-Dev/Arkael-GKI-Builder"
+RELEASE_REPO="Arkael-Dev/Arkael-GKI-Builder/releases"
 
 # ==========================================
 # 5. Default Build Options
